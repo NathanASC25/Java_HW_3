c9c0f64299569eb6a3d776349fb27e6a8b10a2c6
NathanASC25 <javanathanjchin@gmail.com>
2025-11-21 06:09:01 -0500

    fourth commit

--- InOut.java
+++ InOut.java
@@ -3,25 +3,25 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 public class InOut {
     public static void main(String[] args) throws Exception {
-	File inputFile = new File("./TextFiles/inputs.txt");
-	File sumFile = new File("./TextFiles/sum.txt");
-	PrintWriter inputWrite = new PrintWriter(inputFile);
-	PrintWriter sumWrite = new PrintWriter(sumFile);
-	Scanner takeNums = new Scanner(System.in);
-	for (int i = 0; i < 2; i++) {
-	    System.out.println("Enter an integer value: ");
-	    int num = takeNums.nextInt();
-	    inputWrite.println(num);
-	}
-	takeNums.close();
-	inputWrite.close();
-	Scanner readNums = new Scanner(inputFile);
-	int total = 0;
-	while (readNums.hasNext()) {
-	    total += readNums.nextInt();
-	}
-	sumWrite.printf("Sum: %d", total);
-	readNums.close();
-	sumWrite.close();
+        File inputFile = new File("./TextFiles/inputs.txt");
+        File sumFile = new File("./TextFiles/sum.txt");
+        PrintWriter inputWrite = new PrintWriter(inputFile);
+        PrintWriter sumWrite = new PrintWriter(sumFile);
+        Scanner takeNums = new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Enter an integer value: ");
+            int num = takeNums.nextInt();
+            inputWrite.println(num);
+        }
+        takeNums.close();
+        inputWrite.close();
+        Scanner readNums = new Scanner(inputFile);
+        int total = 0;
+        while (readNums.hasNext()) {
+            total += readNums.nextInt();
+        }
+        sumWrite.printf("Sum: %d", total);
+        readNums.close();
+        sumWrite.close();
     }
 }
--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class SumOfDigits {
     public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	// Assume integer input is positive
-	System.out.printf("\nEnter a positive integer: \n");
-	int inputNum = scan.nextInt();
-	int moduloDivNum = 10;
-	int total = 0;
+        Scanner scan = new Scanner(System.in);
+        // Assume integer input is positive
+        System.out.printf("\nEnter a positive integer: \n");
+        int inputNum = scan.nextInt();
+        int moduloDivNum = 10;
+        int total = 0;
 
-	while (inputNum % moduloDivNum != inputNum) {
-	    moduloDivNum *= 10;
-	}
+        while (inputNum % moduloDivNum != inputNum) {
+            moduloDivNum *= 10;
+        }
 
-	while (moduloDivNum >= 10) {
-	    int extractedNum = (inputNum % moduloDivNum) / (moduloDivNum / 10);
+        while (moduloDivNum >= 10) {
+            int extractedNum = (inputNum % moduloDivNum) / (moduloDivNum / 10);
             total += extractedNum;
-	    moduloDivNum /= 10;
-	}
-	System.out.printf("\nSum of Digits: %d\n", total);
+            moduloDivNum /= 10;
+        }
+        System.out.printf("\nSum of Digits: %d\n", total);
     }
 }
